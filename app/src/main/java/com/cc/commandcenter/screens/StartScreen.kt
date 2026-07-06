08728e936987d433647c57cefb8a95d8690a0989
cccompantcenter <chantalcampen@gmail.com>
2026-07-06 16:25:43 +0000

    Sprint 1.7: polish StartScreen

--- app/src/main/java/com/cc/commandcenter/screens/StartScreen.kt
+++ app/src/main/java/com/cc/commandcenter/screens/StartScreen.kt
@@ -5,15 +5,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.cc.commandcenter.components.CcLogo
 import com.cc.commandcenter.components.CcPrimaryButton
@@ -35,7 +35,7 @@ fun StartScreen(
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             CcLogo(
-                modifier = Modifier.size(320.dp)
+                modifier = Modifier.size(340.dp)
             )
 
             Column(
@@ -55,6 +55,22 @@ fun StartScreen(
                     onClick = onOpenDashboard
                 )
             }
+        }
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomEnd),
+            horizontalAlignment = Alignment.End
+        ) {
+            Text(
+                text = "Command Center",
+                textAlign = TextAlign.End
+            )
+
+            Text(
+                text = "Alpha 0.7",
+                textAlign = TextAlign.End
+            )
         }
     }
 }
