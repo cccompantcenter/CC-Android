8dfa9c624660c7eddffe291911a6e895cca00fcf
cccompantcenter <chantalcampen@gmail.com>
2026-07-06 04:43:31 +0000

    Add reusable CC logo component

---
+++ app/src/main/java/com/cc/commandcenter/components/CcLogo.kt
@@ -0,0 +1,20 @@
+package com.cc.commandcenter.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.cc.commandcenter.R
+
+@Composable
+fun CcLogo(
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = painterResource(id = R.drawable.cc_design_reference),
+        contentDescription = "CC Command Center",
+        modifier = modifier.height(72.dp)
+    )
+}
